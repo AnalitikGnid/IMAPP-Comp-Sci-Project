7523631e76d2aecd7d655e70b6c6a096715ea994
KalKoenkie <134587796+KalKoenkie@users.noreply.github.com>
2026-01-21 15:07:36 +0100

    Getting rid of a warning message

--- Project_code/main.cpp
+++ Project_code/main.cpp
@@ -95,5 +95,5 @@ int main() {
     output_file.close();
     image.saveToFile("mandelbrot.png");
 
-    system("python3 create_graphs.py"); // Call the Python script to create the plot
+    (void) system("python3 create_graphs.py"); // Call the Python script to create the plot. (void) to suppress unused return value warning
 }
