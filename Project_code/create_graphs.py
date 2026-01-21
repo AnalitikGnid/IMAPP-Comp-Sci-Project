5c6f1b1a51ae80625d1ad704189343b60108224f
KalKoenkie <134587796+KalKoenkie@users.noreply.github.com>
2026-01-21 15:57:54 +0100

    Change subplot

--- Project_code/create_graphs.py
+++ Project_code/create_graphs.py
@@ -12,18 +12,24 @@ with open('/workspace/grain_size_times.txt', 'r') as file:
         number_of_tasks.append(int(task_count))
 
 # Plot
-plt.figure()
-plt.subplot(2, 1, 1)
-plt.plot(grain_sizes, number_of_tasks, marker='o', color='orange')
-plt.xlabel("Grain size")
-plt.ylabel("Number of tasks (log scale)")
-plt.yscale("log")
-plt.title("Grain size vs Number of Tasks (1200x1200, 2 cores)")
-plt.subplot(2, 1, 2)
-plt.plot(grain_sizes, times, marker='o')
-plt.xlabel("Grain size")
-plt.ylabel("Time taken (microseconds)")
-plt.title("Grain size vs Execution Time (1200x1200, 2 cores)")
-plt.show()
+# Plot time vs grain size and tasks vs grain size in horizontal subplots
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 5))
 
-plt.savefig('/workspace/time_vs_grain_size.png', dpi=120)
+ax1.plot(grain_sizes, times, marker='o')
+ax1.set_xlabel("Grain Size")
+ax1.set_ylabel("Time Taken (microseconds)")
+ax1.set_title("Time vs Grain Size (1200x1200 image, 2 cores)")
+ax1.set_yscale("log")
+ax1.set_xscale("log")
+ax1.grid(True)
+
+ax2.plot(grain_sizes, number_of_tasks, marker='o')
+ax2.set_xlabel("Grain Size")
+ax2.set_ylabel("Number of Tasks (log scale)")
+ax2.set_yscale("log")
+ax2.set_xscale("log")
+ax2.set_title("Tasks vs Grain Size (1200x1200 image, 2 cores)")
+ax2.grid(True)
+
+plt.tight_layout()
+plt.show()
